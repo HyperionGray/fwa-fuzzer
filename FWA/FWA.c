7156207c6ae91017121a8b1fa0265d4fced6ca8a
Alex <acaceres@hyperiongray.com>
2020-10-27 00:10:17 -0700

    adding some bit stuff

--- FWA/FWA.c
+++ FWA/FWA.c
@@ -1,12 +1,16 @@
 #include "fwa.h"
 #include <stdio.h>
+#include <xkeycheck.h>
+#include <time.h>
+#include <stdlib.h>
 
-
-char *read_file_to_memory(const char *filename[]) {
+struct FileWLength read_file_to_memory(const char *filename[]) {
 
 	char* buffer = 0;
 	long length;
 	FILE* f = fopen(filename, "rb");
+	struct FileWLength file_w_length;
+
 
 	if (f)
 	{
@@ -19,26 +23,27 @@ char *read_file_to_memory(const char *filename[]) {
 			fread(buffer, 1, length, f);
 		}
 		fclose(f);
-	}
+	};
+
 	if (buffer)
 	{
-		return buffer;
-	}
+		file_w_length.file_buffer = buffer;
+		file_w_length.length = length;
+		return file_w_length;
+	};
 }
 
+int random(int min, int max) {
+	return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
 
-int bit_flipper(char *arch[], unsigned int position) {
+int file_bit_flipper(unsigned int byte) {
 
-	if (arch == "x86") {
-		BYTE byte = 0xFFFFFFFF;
-	}
-
-
-	if (arch == "x64") {
-	BYTE byte = 0xFFFFFFFFFFFFFFFF;
-
-
-	}
+	//can probably be written in assembly
+	rand() % (65 + 1 - 0) + 0;
+	int mask = 0x90;
+	int num = byte; // 11100011
+	num ^= mask;
 
 }
 
--- FWA/fwa.h
+++ FWA/fwa.h
@@ -7,3 +7,9 @@ int bitflip;
 
 //types
 typedef unsigned int BYTE;
+
+//structs
+struct FileWLength {
+	char *file_buffer;
+	int length;
+};
